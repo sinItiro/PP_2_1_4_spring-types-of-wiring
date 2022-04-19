f047bf9cabbecceac27c08f89ace538b91c52417
Itiro <sinitiro@gmail.com>
2022-04-19 19:46:29 +0300

    2.1.4

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,21 @@
 package koschei.models;
 
-public class Egg6 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Egg6 {
+    private Needle7 needle;
+
+    @Autowired
+    public void setOcean(Needle7 needle) {
+        this.needle = needle;
+    }
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
+
+
+
 }
